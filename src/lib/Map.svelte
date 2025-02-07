392192125d625549f3c842f6de680820e1de340e
Lukas Prader <lukas.prader@tutanota.com>
2025-02-08 00:19:04 +0100

    Map: switch to DSA 5 map, correct CRS with scale and watermark

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,29 +1,56 @@
 <script lang="ts">
-	import { onMount, onDestroy, setContext, createEventDispatcher, tick } from 'svelte';
+	import { onMount, onDestroy, setContext} from 'svelte';
 	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
-
-  export let bounds: [[number, number], [number, number]] = [[0, 0], [1800, 1100]];
-
 
 	let map: L.Map | undefined;
 	let mapElement: HTMLElement;
 
+  // NOTE: geographical bounds taken from dereglobus
+  var corner1 = L.latLng(13.83211470255367, -8.928795211729799),
+  corner2 = L.latLng(44.68523182173993, 13.82951187257615),
+  bounds = L.latLngBounds(corner1, corner2);
+
 	onMount(() => {
 
 		map = L.map(mapElement, {
-        crs: L.CRS.Simple, // Use a simple coordinate system
-        minZoom: 0,
-        maxZoom: 10,
-        zoom: 3,
+        crs: L.CRS.EPSG4326,
+        minZoom: 4,
+        maxZoom: 7,
     });
 
-    const imageUrl = '00_Aventurien.jpg'; // Ensure it's placed in the "static" folder
+    const imageUrl = 'DSA_Karte_Aventurien_A3.png'; // NOTE: converted from the original .pdf file
 
     // Add image overlay as a layer
-    L.imageOverlay(imageUrl, bounds).addTo(map);
+    L.imageOverlay(imageUrl, bounds,
+    {attribution: '&copy; <a href="https://ulisses-spiele.de/downloads/">Ulisses Spiele</a>'}
+    ).addTo(map);
 
-	});
+    L.control.scale({
+      metric: true,
+      imperial: false,
+      position: 'bottomleft'
+    }).addTo(map)
+
+    L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+        img.src = 'Fan-Projekt-Logo.png';
+        img.style.width = '150px';
+        return img;
+    },
+    onRemove: function(map) {
+
+    }
+    });
+
+    L.control.watermark = function(opts) {
+        return new L.Control.Watermark(opts);
+    }
+
+    L.control.watermark({ position: 'topright' }).addTo(map);
+
+});
 
 	onDestroy(() => {
 		map?.remove();
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import '../../app.css';
-	// import type { LatLngExpression } from 'leaflet';
-	import Leaflet from '$lib/Map.svelte';
-
-	const bounds: [[number, number], [number, number]] = [[0, 0], [1800, 1100]]
+	import Map from '$lib/Map.svelte';
 </script>
 
 <div class="map-container">
-	<Leaflet bounds={bounds}>
-	</Leaflet>
+	<Map />
 </div>
 
 <style>
@@ -16,11 +12,5 @@
 		width: 100%;
 		height: 100%;
 		position: relative;
-	}
-
-	/* Ensure the Leaflet map has a height */
-	:global(.leaflet-container) {
-		width: 100%;
-		height: 100%;
 	}
 </style>
