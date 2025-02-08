a9c65b5a97bcb57d000564deac0914ee095e305a
Lukas Prader <lukas.prader@tutanota.com>
2025-02-08 23:41:08 +0100

    add fan project map as second layer

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -3,26 +3,50 @@
 	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
 	import Markers from '$lib/Markers.svelte';
+	// import 'leaflet-ruler';
 
 	let map: L.Map | undefined;
 	let mapElement: HTMLElement;
 
-	var corner1 = L.latLng(13.83211470255367, -8.928795211729799),
-		corner2 = L.latLng(44.68523182173993, 13.82951187257615),
-		bounds = L.latLngBounds(corner1, corner2);
+	var boundsPackage = L.latLngBounds(
+		L.latLng(13.83211470255367, -8.928795211729799),
+		L.latLng(44.68523182173993, 13.82951187257615)
+	);
+
+	var boundsTDE5 = L.latLngBounds(
+		// NOTE: different bounds since the maps are not scaled the same
+		L.latLng(13.83211470255367 - 0.99755, -8.928795211729799 - 2.30301),
+		L.latLng(44.68523182173993 + 1.52704, 13.82951187257615 + 1.74868)
+	);
 
 	onMount(() => {
+		// NOTE: Official TDE 5 map, converted from .pdf.
+		var layerTDE5 = L.imageOverlay('map_layers/DSA_Karte_Aventurien_A3.jpg', boundsTDE5, {
+			attribution: '&copy; <a href="https://ulisses-spiele.de/downloads/">Ulisses Spiele</a>'
+		});
+		// NOTE: Combined map from Ulisses Map package.
+		var layerFan = L.imageOverlay(
+			'map_layers/Aventurien (Regionalkarten) V1.2.jpg',
+			boundsPackage,
+			{
+				attribution:
+					'&copy; <a href="https://www.orkenspalter.de/filebase/index.php?file/46-aventurienkarte-aus-regionalpl%C3%A4nen/">Kartenpaket</a>'
+			}
+		);
+
 		map = L.map(mapElement, {
 			crs: L.CRS.EPSG4326,
 			minZoom: 4,
-			maxZoom: 7
+			maxZoom: 8,
+			layers: [layerTDE5]
 		});
 
-		const imageUrl = 'map_layers/DSA_Karte_Aventurien_A3.png';
+		var baseMaps = {
+			'Free for v5': layerTDE5,
+			'Fan project v.1.2': layerFan
+		};
 
-		L.imageOverlay(imageUrl, bounds, {
-			attribution: '&copy; <a href="https://ulisses-spiele.de/downloads/">Ulisses Spiele</a>'
-		}).addTo(map);
+		L.control.layers(baseMaps).addTo(map);
 
 		L.control.scale({ metric: true, imperial: false, position: 'bottomleft' }).addTo(map);
 
@@ -40,7 +64,7 @@
 			return new L.Control.Watermark(opts);
 		};
 
-		L.control.watermark({ position: 'topright' }).addTo(map);
+		L.control.watermark({ position: 'bottomright' }).addTo(map);
 
 		// Provide the map instance to child components
 		setContext('map', { getMap: () => map });
@@ -52,7 +76,7 @@
 	});
 
 	$: if (map) {
-		map.fitBounds(bounds);
+		map.fitBounds(boundsTDE5);
 	}
 </script>
 
